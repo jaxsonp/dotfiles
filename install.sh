62728c036320cf19a808864421cc76392fbf17d2
Jaxson Pahukula <jaxpahu@gmail.com>
2023-10-10 12:27:37 -0400

    add cronjob to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
 cd $HOME
+# cloning repository
 git clone https://github.com/jaxsonp/dotfiles.git
-bash ./.dotfiles/.bin/sync-dotfiles
+
+# adding cron job 
+(crontab -l; echo "* 0 * * * $HOME/.dotfiles/.bin/sync-dotfiles") | awk '!x[$0]++' | crontab -
+
+# performing manual sync
+sh ./.dotfiles/.bin/sync-dotfiles
 echo "done"
