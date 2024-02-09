4c94919c6b5a0622531e593133533a3550bb3ac7
Jaxson Pahukula <jaxpahu@gmail.com>
2024-02-09 17:50:37 -0500

    Sync Fri Feb  9 05:50:37 PM EST 2024

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,31 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"bluz71/vim-nightfly-colors",
+		name = "nightfly",
+		lazy = false,
+		priority = 1000
+	},
+}, {})
+
 
 -- appearance stuff
+vim.o.termguicolors = true
+vim.cmd [[colorscheme nightfly]]
 vim.o.number = true
 
 -- whitespace
