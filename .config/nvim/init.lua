48c7a44a190a76ae3e195c86c9f3c092ad559402
Jaxson Pahukula <jaxpahu@gmail.com>
2024-02-11 17:55:47 -0500

    Sync Sun Feb 11 05:55:47 PM EST 2024

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,8 @@
 
+-- disabling default file explorer for nvim-tree
+vim.g.loaded_netrw = false
+vim.g.loaded_netrwPlugin = false
+
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -12,19 +16,63 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
 require("lazy").setup({
-	{
-		"rebelot/kanagawa.nvim"
+	{ -- color scheme
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function ()		
+			require("rose-pine").setup({
+				variant = "main",
+				dark_variant = "main",
+				dim_inactive_windows = true,
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = false,
+				}
+			})
+		end,
+	},
+	{ -- tree sitter
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function ()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				highlight = { enable = true },
+				indent = { enable = true},
+			})
+		end
+	},
+	{ -- indent guides 
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl", 
+		opts = {}
+	},
+	{ -- file explorer
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		},
+		config = function ()
+			require("nvim-tree").setup {}
+		end,
 	},
 }, {})
 
 
 -- appearance stuff
+vim.opt.background = "dark"
+vim.cmd("colorscheme rose-pine")
 vim.cmd [[set termguicolors]]
-vim.cmd [[colorscheme kanagawa]]
 vim.o.number = true
 
 -- whitespace
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+
+-- behavior
+vim.o.autoindent = true
+vim.o.smartindent = true
