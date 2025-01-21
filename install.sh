862ade76864fd07287cf55b1cc81327f26834755
Jaxson Pahukula <jaxpahu@gmail.com>
2025-01-20 23:12:33 -0500

    Sync Mon Jan 20 23:12:33 EST 2025

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ cd $HOME
 git clone https://github.com/jaxsonp/dotfiles.git
 mv dotfiles/ .dotfiles/
 
+# perms
+chmod 700 -r ./.dotfiles
+
 # adding cron job 
 (crontab -l; echo "* 0 * * * $HOME/.dotfiles/bin/sync-dotfiles") | awk '!x[$0]++' | crontab -
 
