de74bba954eda8a7a2fc9e5e9bb459d825a17f25
Jaxson Pahukula <jaxpahu@gmail.com>
2025-04-28 01:09:57 -0400

    improvements

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,39 +35,22 @@ vim.opt.confirm = true
 -- what's included in a session
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
-
 -- leader commands
 
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<Leader>w', ':w<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>w', ':w<CR>', { desc = "Write buffer shortcut", noremap = true })
+vim.keymap.set('n', '<Leader>n', ':noh<CR>', { desc = "No highlight shortcut", noremap = true })
+vim.keymap.set('n', '<Leader>==', 'gg=G\'\'', { desc = "Global fix indentation shortcut", noremap = true })
 
-vim.keymap.set('n', '<Leader>==', 'gg=G\'\'', { noremap = true, silent = true })
-
-vim.keymap.set('n', '<Leader>b', ':lua require("telescope.builtin").buffers({})<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>d', ':lua require("telescope.builtin").diagnostics({ bufnr=0 })<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>ff', ':lua require("telescope.builtin").find_files()<CR>', { noremap = true, silent = true })
-
--- telescope git_files if in git repo, otherwise find_files
-local is_inside_git_tree = {}
-vim.keymap.set('n', '<leader>g', function()
-	local opts = {}
-	local cwd = vim.fn.getcwd()
-	if is_inside_git_tree[cwd] == nil then
-		vim.fn.system("git rev-parse --is-inside-work-tree")
-		is_inside_git_tree[cwd] = vim.v.shell_error == 0
-	end
-	if is_inside_git_tree[cwd] then
-		require("telescope.builtin").git_files(opts)
-	else
-		require("telescope.builtin").find_files(opts)
-	end
-end, { noremap = true })
-
-vim.keymap.set('n', '<Leader>tt', ':lua require("nvim-tree.api").tree.toggle({ path=vim.fn.getcwd() })<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>tf', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>tr', ':NvimTreeRefresh<CR>', { noremap = true, silent = true })
-
+-- show diagnositcs
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = false,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
 
 
 -- plugins
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"EdenEast/nightfox.nvim",
-		lazy = false, -- load always
 		priority = 1000, -- load first
 		config = function()
 			vim.cmd([[colorscheme carbonfox]])
@@ -55,48 +54,99 @@ return {
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function ()
-			require('telescope').setup{}
-		end
+		opts = {
+			defaults = {},
+			pickers = {
+				find_files = {
+					attach_mappings = function(prompt_bufnr, map)
+						-- function to toggle hidden files on ctrl h
+						local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+						local hidden = false
+
+						local function toggle_hidden()
+							hidden = not hidden
+							picker:refresh(
+								require('telescope.finders').new_oneshot_job(
+									{ "rg", "--files", hidden and "--hidden" or nil },
+									{ entry_maker = require('telescope.make_entry').gen_from_file() }
+								),
+								{ reset_prompt = true }
+							)
+							vim.notify(hidden and "Toggled hidden files ON" or "Toggled hidden files OFF")
+						end
+
+						map("i", "<C-h>", toggle_hidden)
+						map("n", "<C-h>", toggle_hidden)
+
+						return true
+					end,
+				},
+				buffers = {
+					mappings = {
+						n = {
+							["<Del>"] = require('telescope.actions').delete_buffer,
+						}
+					},
+					sort_lastused = true,
+				},
+				diagnostics = {
+					layout_strategy = "vertical",
+				}
+			}
+		},
+		keys = {
+			{ '<Leader>b', function() require("telescope.builtin").buffers() end, desc = "[B]uffers", noremap = true },
+			{ '<Leader>d', function() require("telescope.builtin").diagnostics({ bufnr=0 }) end, desc = "[D]iagnostics",  noremap = true },
+			{ '<Leader>ff', function() require("telescope.builtin").find_files() end, desc = "[F]ind [F]iles",noremap = true },
+			{ '<Leader>fa', function() require("telescope.builtin").find_files({ no_ignore=true }) end, desc = "[F]ind [A]ll files (don't respect gitignore)",noremap = true },
+			{ '<Leader>fg', function() require("telescope.builtin").git_files() end, desc = "[F]ind [G]it files",noremap = true },
+		}
 	},
 	{
 		'rmagatti/auto-session',
 		lazy = false,
 		opts = {
-			suppressed_dirs = { '~/', '~/Downloads', '/' },
+			suppressed_dirs = { vim.fn.expand("~"), vim.fn.expand("~/Downloads"), "/" },
+			lazy_support = true,
 			show_auto_restore_notif = true,
-		}
+		},
+		keys = {
+			{ '<Leader>ss', ':SessionSave<CR>', desc = "[S]ession [S]ave", noremap = true },
+			{ '<Leader>sr', ':SessionRestore<CR>', desc = "[S]ession [R]estore", noremap = true },
+		},
 	},
 	{
 		'nvim-tree/nvim-tree.lua',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
+		opts = {
+			view = {
+				side = "right",
+				width = 40,
+			},
+			renderer = {
+				icons = {
+					git_placement = "after",
+				}
+			}
+		},
+		key = {
+			{ '<Leader>tt', function() require("nvim-tree.api").tree.toggle({ path=vim.fn.getcwd() }) end, desc = "[T]ree [T]oggle", noremap = true },
+			{ '<Leader>tf', function() require("nvim-tree.api").tree.find_file() end, desc = "[T]ree current [F]ile", noremap = true },
+			{ '<Leader>tr', function() require("nvim-tree.api").tree.reload() end, desc = "[T]ree [R]eload", noremap = true },
+		},
+		init = function()
 			-- gotta disable this cus the readme said
 			vim.g.loaded_netrw = 1
 			vim.g.loaded_netrwPlugin = 1
-
-			require("nvim-tree").setup({
-				view = {
-					side = "right",
-					width = 40,
-				}
-			})
-		end
+		end,
 	},
 
 	-- lsp config stuff
 	{
 		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"pyright",
-			}
-		}
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = true,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -117,7 +167,6 @@ return {
 		end,
 		config = function()
 			-- lsp settings
-
 			require("mason").setup({
 				PATH = "append",
 			})
@@ -140,29 +189,29 @@ return {
 					}
 				}
 			})
-			require('lspconfig').pyright.setup{}
 
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "python", "markdown" },
-				indent = { enable = true },
-			})
+			if vim.lsp.inlay_hint then
+				vim.lsp.inlay_hint.enable(true, { 0 })
+			end
 
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
-		config = function ()
-			require('nvim-treesitter.configs').setup {
-				ensure_installed = { "lua", "rust", "toml", "python" }
-			}
-		 end
+		build = ":TSUpdate", -- update parsers on plugin update
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			ensure_installed = { "lua", "rust", "toml", "python" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
 	},
 	{ 'rust-lang/rust.vim' },
 	{
 		'mrcjkb/rustaceanvim',
 		version = '^6',
-		lazy = false,
-		config = function()
+		ft = 'rust',
+		init = function()
 			vim.g.rustfmt_autosave = true
 		end
 	}
