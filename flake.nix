a77359cebd2b2e9103833e1bd36085989ac037dc
Jaxson Pahukula <jaxpahu@gmail.com>
2025-07-28 23:09:19 -1000

    set up kde stuff

--- flake.nix
+++ flake.nix
@@ -1,21 +1,39 @@
 {
-  description = "My home manager configuration";
+  description = "Home Manager configuration of jaxson";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    inputs@{ nixpkgs, home-manager, plasma-manager, ... }:
     let
-       lib = nixpkgs.lib;
-       system = "x86_64-linux";
-       pkgs = import nixpkgs { inherit system; };
-    in {
-      homeConfigurations = {
-        jaxson = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home.nix ];
-        };
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations.jaxson = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          inputs.plasma-manager.homeManagerModules.plasma-manager
+          ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
       };
     };
 }
